121394da17e50651b303cc54892ff5e5e407ba03
Jure Taslak <jure.taslak@gmail.com>
2017-04-21 22:30:21 +0200

    Creating repository for student project PKP

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the code for the student project
+
+The python files are in the bin folder
---
+++ bin/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Box2D import (b2PolygonShape, b2World)
+
+world = b2World()  # default gravity is (0,-10) and doSleep is True
+
